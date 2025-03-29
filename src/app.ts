53100f44c5f28d02e6a7d9fa5569e4a2d2479ff4
Starry Skadi <starryskadi@gmail.com>
2025-03-29 16:05:35 +0700

    feat: project intialization

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { DoublyLinkNodeList } from "./structure/linked_list/linked_node_list"
+
+const linkedNodeList = new DoublyLinkNodeList()
+
+linkedNodeList.append(1)
+linkedNodeList.append(2)
+linkedNodeList.append(3)
+
+linkedNodeList.reverse()
