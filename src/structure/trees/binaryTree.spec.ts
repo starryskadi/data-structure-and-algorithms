f09c30049daa49f4fecf157c0a8ece44f6351ebe
Starry Skadi <starryskadi@gmail.com>
2025-03-30 09:54:11 +0700

    refactor: binary tree node refactor

--- src/structure/trees/binaryTree.spec.ts
+++ src/structure/trees/binaryTree.spec.ts
@@ -2,7 +2,7 @@ import { BinarySearchTree, BinaryTreeNode } from "./binaryTree"
 
 
 describe('Binary Tree', () => { 
-    it("Binary Tree can be inserted", () => {
+    it("Binary Tree can be inserted and lookup", () => {
         const binarySearchTree = new BinarySearchTree()
         binarySearchTree.insert(50)
         binarySearchTree.insert(30)
@@ -14,5 +14,17 @@ describe('Binary Tree', () => {
         const lookupItem = binarySearchTree.lookup(30)
 
         expect(lookupItem).toEqual(new BinaryTreeNode(30, new BinaryTreeNode(29), new BinaryTreeNode(31)))
+    })
+
+    it("Binary Tree can't be inserted thne same value", () => {
+        const binarySearchTree = new BinarySearchTree()
+        binarySearchTree.insert(50)
+        binarySearchTree.insert(30)
+        binarySearchTree.insert(55)
+
+        binarySearchTree.insert(29)
+        binarySearchTree.insert(31)
+
+        expect(() => binarySearchTree.insert(29)).toThrow()
     })
  })
--- src/structure/trees/binaryTree.ts
+++ src/structure/trees/binaryTree.ts
@@ -16,62 +16,66 @@ export class BinarySearchTree<T> {
 
     constructor() {}
 
-    insert(value: T) {
+    insert(value: T): BinarySearchTree<T> {
         const newNode = new BinaryTreeNode<T>(value)
         if (!this.root) {
             this.root = newNode
-            return
+            return this
         }
 
-        let currentNode: BinaryTreeNode<T> | null = this.root
+        let currentNode: BinaryTreeNode<T> = this.root
 
-        while (currentNode)  {
+        while (true)  {
             if (value === currentNode.value) {
-                currentNode = null
                 throw Error("The node with that value already existed")
             }
             // Decide whether it should put on left - if the value is lower than current node
             else if (value < currentNode.value) {
-                if (currentNode.left && currentNode.left.value) {
-                    currentNode = currentNode.left
-                } else if (!currentNode.left) {
+                if (!currentNode.left) {
                     currentNode.left = newNode
-                    currentNode = null
+                    break
                 }
+
+                currentNode = currentNode.left
             // Decide whether it should put on the right - if the value is exceed than current node 
-            } else if (value > currentNode.value) {
-                if (currentNode.right && currentNode.right.value) {
-                    currentNode = currentNode.right
-                } else if (!currentNode.right) {
+            } else {
+                if (!currentNode.right) {
                     currentNode.right = newNode
-                    currentNode = null
+                    break
                 }
+
+                currentNode = currentNode.right
             }
         }        
+
+        return this
     }
 
-    lookup(value: T) {
-        let currentNode: BinaryTreeNode<T> | null = this.root
+    lookup(value: T): BinaryTreeNode<T> | null {
+        if (!this.root) {
+            throw Error("There is no data in binary tree yet")
+        }
+        let currentNode: BinaryTreeNode<T> = this.root
         let foundNode: BinaryTreeNode<T> | null = null
 
-        while (currentNode)  {
+        while (true)  {
             // Decide whether it should put on left - if the value is lower than current node
             if (value === currentNode.value) {
                 foundNode = currentNode
-                currentNode = null
+                break
             }
             else if (value < currentNode.value) {
                 if (currentNode.left && currentNode.left.value) {
                     currentNode = currentNode.left
                 } else if (!currentNode.left) {
-                    currentNode = null
+                    break
                 }
             // Decide whether it should put on the right - if the value is exceed than current node 
             } else if (value > currentNode.value) {
                 if (currentNode.right && currentNode.right.value) {
                     currentNode = currentNode.right
                 } else if (!currentNode.right) {
-                    currentNode = null
+                    break
                 }
             } 
         }     
